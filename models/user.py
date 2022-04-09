035e5eb3bec2a00b5340b8831f5cc2583a5948e6
dk-Gk <idkin19@gmail.com>
2022-04-09 22:09:30 +0000

    edited

--- models/user.py
+++ models/user.py
@@ -8,22 +8,19 @@ from os import getenv
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    if getenv("HBNB_TYPE_STORAGE") != "db":
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship(
-            "Place",
-            cascade="all,delete, delete-orphan",
-            backref="user",)
-        reviews = relationship(
-            "Review",
-            cascade="all,delete, delete-orphan",
-            backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
+     email = Column(String(128), nullable=False)
+         password = Column(String(128), nullable=False)
+         first_name = Column(String(128), nullable=True)
+         last_name = Column(String(128), nullable=True)
+         places = relationship(
+             "Place",
+             cascade="all,delete",
+             backref=backref("user", cascade="all,delete"),
+             passive_deletes=True,
+             single_parent=True)
+         reviews = relationship(
+             "Review",
+             cascade="all,delete",
+             backref=backref("user", cascade="all,delete"),
+             passive_deletes=True,
+             single_parent=True)
