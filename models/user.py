87ca30a10df54674b4acad1029c94098e862c8db
dk-Gk <idkin19@gmail.com>
2022-04-09 22:11:25 +0000

    edited

--- models/user.py
+++ models/user.py
@@ -9,18 +9,18 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
      email = Column(String(128), nullable=False)
-         password = Column(String(128), nullable=False)
-         first_name = Column(String(128), nullable=True)
-         last_name = Column(String(128), nullable=True)
-         places = relationship(
-             "Place",
-             cascade="all,delete",
-             backref=backref("user", cascade="all,delete"),
-             passive_deletes=True,
-             single_parent=True)
-         reviews = relationship(
-             "Review",
-             cascade="all,delete",
-             backref=backref("user", cascade="all,delete"),
-             passive_deletes=True,
-             single_parent=True)
+     password = Column(String(128), nullable=False)
+     first_name = Column(String(128), nullable=True)
+     last_name = Column(String(128), nullable=True)
+     places = relationship(
+         "Place",
+         cascade="all,delete",
+         backref=backref("user", cascade="all,delete"),
+         passive_deletes=True,
+         single_parent=True)
+     reviews = relationship(
+         "Review",
+         cascade="all,delete",
+         backref=backref("user", cascade="all,delete"),
+         passive_deletes=True,
+         single_parent=True)
